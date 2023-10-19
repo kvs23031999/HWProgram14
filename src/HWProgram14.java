a067ede7103cb44a6f0c25f43017b58feac29fc3
Vlad <147541054+kvs23031999@users.noreply.github.com>
2023-10-19 14:05:36 +0300

    first commit

--- src/HWProgram14.java
+++ src/HWProgram14.java
@@ -1,3 +1,4 @@
+import java.sql.Array;
 import java.util.Arrays;
 import java.util.Random;
 
@@ -6,40 +7,34 @@ public class HWProgram14 {
     public static void main(String[] args) {
 
         Random random = new Random();
-        int[] arrayNum1 = new int[]{1,1};
-        for (int i = 0; i < arrayNum1.length; i++) {
-            arrayNum1[i] = random.nextInt(10);
-        }
-        System.out.println(Arrays.toString(arrayNum1) + " 1 число");
+        int randomNum = random.nextInt(100_000) + 11;
+        System.out.println(randomNum);
+        int sum = 0;
 
-        int [] arrayNum2 = new int[]{1,0,0,0,1,1};
-        for (int i = 0; i < arrayNum2.length; i++) {
-           arrayNum2[i] = random.nextInt(10);
+        for (int i = 0; i < 6; i++) {
+            int partNumber = randomNum%10;
+            randomNum /= 10;
+            sum += partNumber ;
         }
-        System.out.println(Arrays.toString(arrayNum2) + " 2 число");
-        int [] arrayNum3 = new int[]{1,2,3};
-        for (int i = 0; i < arrayNum3.length; i++) {
-            arrayNum3[i] = random.nextInt(10);
-        }
-        System.out.println(Arrays.toString(arrayNum3) + " 3 число");
+        System.out.println("Сума числа" + ": " + sum);
 
-        int sum1 = 0;
-        for (int i = 0; i < arrayNum1.length; i++) {
-            sum1 += arrayNum1[i];
-        }
-        System.out.println("сума 1 числа" + ": " + sum1);
 
-        int sum2 = 0;
-        for (int i = 0; i < arrayNum2.length; i++) {
-            sum2 += arrayNum2[i];
-        }
-        System.out.println("сума 2 числа" + ": " + sum2);
 
-        int sum3 = 0;
-        for (int i = 0; i < arrayNum3.length; i++) {
-           sum3 += arrayNum3[i];
-        }
-        System.out.println("сума 3 числа" + ": " + sum3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
